e66a92d95ba3b4a69e604a781fe8cf6a081487b4
Rudresh HM <rudreshhm2810@gmail.com>
2020-06-02 01:50:07 +0530

    in feature branch

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("nakkan");
+let objParent={
+    name:"rudresh",
+    age:21,
+}
+let objChild={};
+objChild.__proto__=objParent;
+objChild.name="shede";
+console.log(objChild);
+console.log(objParent);
+console.log('mofo');
